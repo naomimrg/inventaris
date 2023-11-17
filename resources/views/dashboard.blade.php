bc9b7cf07bf23223e51ea445f374b285902c3e80
Naomi <naomimanurung40@gmail.com>
2023-11-17 13:34:11 +0700

    nambah api

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,30 +6,51 @@
         }
     </style>
     <div class="row">
-        <div class="col-md-4">
-            <div class="card bg-primary text-white">
+        <div class="col">
+            <div class="card border-left-primary shadow h-100 py-2 mb-4">
                 <div class="card-body">
-                    <i class="fas fa-dollar-sign fa-2x"></i>
-                    <h5 class="card-title">Total Aset</h5>
-                    <div class="card-text">{{$aset_count}}</div>
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Data Aset</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $aset_count }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="card bg-danger text-white">
+        <div class="col">
+            <div class="card border-left-danger shadow h-100 py-2 mb-4">
                 <div class="card-body">
-                    <i class="fas fa-exclamation-circle fa-2x"></i>
-                    <h5 class="card-title">Total Laporan Kerusakan</h5>
-                    <p class="card-text">{{$kerusakan_count}}</p>
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                Total Laporan Kerusakan</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kerusakan_count }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-exclamation-circle fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="card bg-success text-white">
+        <div class="col">
+            <div class="card border-left-success shadow h-100 py-2 mb-4">
                 <div class="card-body">
-                    <i class="fas fa-shopping-cart fa-2x"></i>
-                    <h5 class="card-title">Total Peminjaman Aset</h5>
-                    <p class="card-text">{{$peminjaman_count}}</p>
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Total Laporan Peminjaman</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $peminjaman_count }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\AsetController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\KerusakanController;
+use App\Http\Controllers\LokasiController;
+use App\Http\Controllers\PeminjamanController;
+use App\Http\Controllers\PerbaikanController;
+use App\Http\Controllers\RiwayatController;
+use App\Http\Controllers\SesiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +23,73 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [SesiController::class, 'login']);
+Route::post('/register', [SesiController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Rute API untuk Aset
+    Route::prefix('asets')->group(function () {
+        Route::get('', [AsetController::class, 'index']);
+        Route::get('{id}', [AsetController::class, 'show']);
+        Route::post('create', [AsetController::class, 'store']);
+        Route::put('update/{id}', [AsetController::class, 'update']);
+        Route::delete('delete/{id}', [AsetController::class, 'destroy']);
+    });
+
+    // Rute API untuk Kerusakan
+    Route::prefix('kerusakans')->group(function () {
+        Route::get('', [KerusakanController::class, 'index']);
+        Route::get('{id}', [KerusakanController::class, 'show']);
+        Route::post('create', [KerusakanController::class, 'store']);
+        Route::put('update/{id}', [KerusakanController::class, 'update']);
+        Route::delete('delete/{id}', [KerusakanController::class, 'destroy']);
+    });
+
+    // Rute API untuk Peminjaman
+    Route::prefix('peminjamans')->group(function () {
+        Route::get('', [PeminjamanController::class, 'index']);
+        Route::get('{id}', [PeminjamanController::class, 'show']);
+        Route::post('create', [PeminjamanController::class, 'store']);
+        Route::put('update/{id}', [PeminjamanController::class, 'update']);
+        Route::delete('delete/{id}', [PeminjamanController::class, 'destroy']);
+    });
+
+    // Rute API untuk Kategori
+    Route::prefix('kategoris')->group(function () {
+        Route::get('', [KategoriController::class, 'index']);
+        Route::get('{id}', [KategoriController::class, 'show']);
+        Route::post('create', [KategoriController::class, 'store']);
+        Route::put('update/{id}', [KategoriController::class, 'update']);
+        Route::delete('delete/{id}', [KategoriController::class, 'destroy']);
+    });
+
+    // Rute API untuk Lokasi
+    Route::prefix('lokasis')->group(function () {
+        Route::get('', [LokasiController::class, 'index']);
+        Route::get('{id}', [LokasiController::class, 'show']);
+        Route::post('create', [LokasiController::class, 'store']);
+        Route::put('update/{id}', [LokasiController::class, 'update']);
+        Route::delete('delete/{id}', [LokasiController::class, 'destroy']);
+    });
+
+    // Rute API untuk Perbaikan
+    Route::prefix('perbaikans')->group(function () {
+        Route::get('', [PerbaikanController::class, 'index']);
+        Route::get('{id}', [PerbaikanController::class, 'show']);
+        Route::post('create', [PerbaikanController::class, 'store']);
+        Route::put('update/{id}', [PerbaikanController::class, 'update']);
+        Route::delete('delete/{id}', [PerbaikanController::class, 'destroy']);
+    });
+
+    // Rute API untuk Riwayat
+    Route::prefix('riwayats')->group(function () {
+        Route::get('', [RiwayatController::class, 'index']);
+        Route::get('{id}', [RiwayatController::class, 'show']);
+        Route::post('create', [RiwayatController::class, 'store']);
+        Route::put('update/{id}', [RiwayatController::class, 'update']);
+        Route::delete('delete/{id}', [RiwayatController::class, 'destroy']);
+    });
 });

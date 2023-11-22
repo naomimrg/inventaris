09647c9f724234ea429f8d75e3d794c519d9a8d7
Naomi <naomimanurung40@gmail.com>
2023-11-22 20:12:15 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,55 +4,133 @@
         .card-body .fas {
             margin-bottom: 3px;
         }
+
+        .chart-container {
+            margin-top: 20px;
+        }
+
+        .chart {
+            width: 100%;
+            height: 30px;
+            background-color: #f0f0f0;
+            border-radius: 5px;
+            overflow: hidden;
+            margin-bottom: 10px;
+        }
+
+        .bar {
+            height: 100%;
+        }
+
+        .good {
+            background-color: #4CAF50; 
+        }
+
+        .average {
+            background-color: #FFC107; 
+        }
+
+        .bad {
+            background-color: #e54141;
+        }
+
+        .condition-title {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .condition-text {
+            font-size: 14px;
+            margin-bottom: 5px;
+        }
+
+        .separator {
+            border-top: 2px solid #ddd;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
     </style>
-    <div class="row">
-        <div class="col">
-            <div class="card border-left-primary shadow h-100 py-2 mb-4">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Total Data Aset</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $asetCount }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card border-left-primary shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Total Data Aset</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $asetCount }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="card border-left-danger shadow h-100 py-2 mb-4">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                Total Laporan Kerusakan</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kerusakanCount }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-exclamation-circle fa-2x text-gray-300"></i>
+
+            <div class="col-md-4">
+                <div class="card border-left-danger shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-danger">Total Laporan Kerusakan</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $kerusakanCount }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-exclamation-circle fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="card border-left-success shadow h-100 py-2 mb-4">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Total Laporan Peminjaman</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $peminjamanCount }}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
+
+            <div class="col-md-4">
+                <div class="card border-left-success shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-success">Total Laporan Peminjaman</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $peminjamanCount }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="col-md-12 separator"></div>
+
+            {{-- <div class="col-md-12">
+                <div class="condition-title">Daftar Aset</div>
+                <div class="chart-container">
+                    <div class="condition-text">Kondisi Baik</div>
+                    <div class="chart">
+                        <div class="bar good" style="width: 60%;"></div>
+                    </div>
+                </div>
+                <div class="chart-container">
+                    <div class="condition-text">Kondisi Kurang Baik</div>
+                    <div class="chart">
+                        <div class="bar average" style="width: 30%;"></div>
+                    </div>
+                </div>
+                <div class="chart-container">
+                    <div class="condition-text">Kondisi Buruk</div>
+                    <div class="chart">
+                        <div class="bar bad" style="width: 10%;"></div>
+                    </div>
+                </div>
+            </div> --}}
         </div>
     </div>
 @endsection
